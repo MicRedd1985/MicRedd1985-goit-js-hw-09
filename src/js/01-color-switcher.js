f3b8b3622a7f4801dce644b20527c82f773d0051
MicRedd1985 <micredd1985@gmail.com>
2022-10-31 18:20:10 +0200

    зроблено дз (1,2,3 завдання)

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const btnStart = document.querySelector ("button[data-start]"); 
+const btnStop = document.querySelector ('button[data-stop]');
+const body = document.querySelector ('body')
 
+const base = {
+    intervalId : null,
+    isActive: false, 
+
+    start() {
+        if (this.isActive) {
+            return;
+        }
+        this.isActive = true;
+        this.intervalId = setInterval(() => {
+            body.style.backgroundColor = getRandomHexColor();
+            console.log (this.intervalId)
+        },1000);
+        console.log ('start')
+
+        
+    },
+
+    stop () {
+        clearInterval (this.intervalId)
+        this.isActive = false;
+        console.log ('stop')
+
+    }
+}
+
+btnStart.addEventListener ('click', () => {base.start ()});
+btnStop.addEventListener ('click', () => {base.stop ()});
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
---
+++ src/js/02-timer.js
@@ -0,0 +1,78 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const btnStart = document.querySelector ('button[data-start]');
+const timerId = document.querySelector ('timer');
+
+
+const dataDays = document.querySelector ('span[data-days]')
+const dataHours = document.querySelector ('span[data-hours]')
+const dataMinutes = document.querySelector ('span[data-minutes]')
+const dataSeconds = document.querySelector ('span[data-seconds]')
+
+function addLeadingZero(value){
+    return value.toString().padStart(2,'0');
+  }
+  let btnActive = false
+
+
+function convertMs(ms) {
+    let data
+    // Number of milliseconds per unit of time (from Zad#2 DZ#9)
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+  
+    // Remaining days
+    const days = addLeadingZero(Math.floor(ms / day));
+    // Remaining hours
+    const hours = addLeadingZero(Math.floor((ms % day) / hour));
+    // Remaining minutes
+    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+    // Remaining seconds
+    const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+  
+  
+    return data = { days, hours, minutes, seconds };
+  }
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        let nowDate = new Date()
+        if (selectedDates[0] < nowDate) {
+          return alert("Please choose a date in the future")
+        }
+        btnStart.addEventListener('click', onClickStart)
+      }
+      }
+      const fltpckr = flatpickr("#datetime-picker", options)
+
+      function onClickStart() {
+        if (btnActive) {
+              return;
+        }
+        btnActive = true;
+        console.log('start')
+        setInterval(() => {
+          let nowDate = new Date()
+          ms = fltpckr.selectedDates[0] - nowDate;
+          const timeElements = convertMs(ms)
+          console.log('countdown ->', timeElements)
+          console.log(timeElements.days)
+          dataDays.textContent = timeElements.days
+          dataHours.textContent = timeElements.hours
+          dataMinutes.textContent = timeElements.minutes
+          dataSeconds.textContent = timeElements.seconds
+        }, 1000)
+      }
+  
+
+    
+
+      
